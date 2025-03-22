a12df9caf9974ce83b847a3c79a210089fc0f635
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-03-22 14:03:12 +0530

    Added setup for project and wrote code for textinputs

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TextInputForm from "./components/TextInputForm/TextInputForm";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-semibold text-3xl">Welcome to Hangman</h1>
+      <TextInputForm />
+    </div>
+  );
+}
+
+export default App;
