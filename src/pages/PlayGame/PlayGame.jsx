34223904d3b7acc1da6f5aa4dede67bf5e050e3d
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-03-26 20:01:43 +0530

    Added masked text function

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
- 
- function PlayGame() {
- 
-     return (
-         <div>
-             <h1>Play Game</h1>
- 
-             <Link to='/start'> Start Game </Link>
-         </div>
-     );
- }
- export default PlayGame;
+import MaskedText from "../../components/MaskedText/MaskedText";
+
+function PlayGame() {
+
+    return (
+        <div>
+            <h1>Play Game</h1>
+            
+            <MaskedText text="humble" usedLetters={['b', 'e']} />
+            <hr/>
+
+            <Link to='/start'> Start Game </Link>
+        </div>
+    );
+}
+export default PlayGame;
